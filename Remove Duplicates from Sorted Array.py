bada45040f4cf1ce87db847cd683544e389acba0
moongar <moosa.moghimi@gmail.com>
2020-03-14 23:19:33 -0600

    Second solution. Fast runtime, low memory usage

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -1,16 +1,29 @@
 def removeDuplicates(nums):
-    #solution 1 O(n)
-    count = 0
-    my_dict = {}
-    for n in nums:
-        if n not in my_dict.keys():
-            my_dict[n] = 1
-            nums[count] = n
-            count += 1
+    # solution 1 O(n)
+    # count = 0
+    # my_dict = {}
+    # for n in nums:
+    #     if n not in my_dict.keys():
+    #         my_dict[n] = 1
+    #         nums[count] = n
+    #         count += 1
 
-    return count
+    # second solution O(n)
+    if len(nums) == 0:
+        return 0
+    else:
+        count = 1
+        current = nums[0]
+        for idx in range(1, len(nums)):
+            if nums[idx] > current:
+                current = nums[idx]
+                nums[count] = nums[idx]
+                count += 1
+
+        return count
 
 
-nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+# nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+nums = []
 print(removeDuplicates(nums))
 print(nums)
