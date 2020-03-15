79bbc4a7332de24061aca6b40ac6806b451f5314
moongar <moosa.moghimi@gmail.com>
2020-03-14 21:37:49 -0600

    faster solution using a dictionary

--- two sum.py
+++ two sum.py
@@ -1,14 +1,23 @@
 def twoSum(nums, target):
-    l = len(nums)
-    for idx1 in range(0, l - 1):
-        res = target - nums[idx1]
-        for idx2 in range(idx1 + 1, l):
-            if res == nums[idx2]:
-                return [idx1, idx2]
+    # first solution O(n^2)
+    # l = len(nums)
+    # for idx1 in range(0, l - 1):
+    #     res = target - nums[idx1]
+    #     for idx2 in range(idx1 + 1, l):
+    #         if res == nums[idx2]:
+    #             return [idx1, idx2]
+
+    # second solution O(n)
+    my_dict = {}
+    for idx in range(len(nums)):
+        rem = target - nums[idx]
+        if rem in my_dict.keys():
+            return [my_dict[rem], idx]
+        my_dict[nums[idx]] = idx
 
     return None
 
 
 num_array = [9, 7, 11, 15]
-target_num = 18
+target_num = 16
 print(twoSum(num_array, target_num))
