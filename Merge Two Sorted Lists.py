4b1f0260208e37d13f4de1a72f3acba46d4f88a0
moongar <moosa.moghimi@gmail.com>
2020-03-15 22:29:46 -0600

    Second solution with singly linked lists

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -1,24 +1,52 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 def mergeTwoLists(l1, l2):
-    merged_list = []
-    l1_idx = 0
-    l2_idx = 0
-    while l1_idx < len(l1) or l2_idx < len(l2):
-        if l1_idx == len(l1):
-            merged_list.append(l2[l2_idx])
-            l2_idx += 1
-        elif l2_idx == len(l2):
-            merged_list.append(l1[l1_idx])
-            l1_idx += 1
+    # first solution for lists O(n)
+    # merged_list = []
+    # l1_idx = 0
+    # l2_idx = 0
+    # while l1_idx < len(l1) or l2_idx < len(l2):
+    #     if l1_idx == len(l1):
+    #         merged_list.append(l2[l2_idx])
+    #         l2_idx += 1
+    #     elif l2_idx == len(l2):
+    #         merged_list.append(l1[l1_idx])
+    #         l1_idx += 1
+    #     else:
+    #         if l1[l1_idx] < l2[l2_idx]:
+    #             merged_list.append(l1[l1_idx])
+    #             l1_idx += 1
+    #         else:
+    #             merged_list.append(l2[l2_idx])
+    #             l2_idx += 1
+    # return merged_list
+    # second solution using singly linked lists O(n)
+    if l1 == None and l2 == None:
+        return None
+    res = ListNode(None)
+    merged_list = res
+    first = True
+    while l1 != None or l2 != None:
+        if not first:
+            merged_list.next = ListNode(None)
+            merged_list = merged_list.next
+        if l1 == None:
+            merged_list.val = l2.val
+            l2 = l2.next
+        elif l2 == None:
+            merged_list.val = l1.val
+            l1 = l1.next
         else:
-            if l1[l1_idx] < l2[l2_idx]:
-                merged_list.append(l1[l1_idx])
-                l1_idx += 1
+            if l1.val < l2.val:
+                merged_list.val = l1.val
+                l1 = l1.next
             else:
-                merged_list.append(l2[l2_idx])
-                l2_idx += 1
-    return merged_list
+                merged_list.val = l2.val
+                l2 = l2.next
+        first = False
 
-
-l1 = [1, 3, 4, 5, 6, 6, 6]
-l2 = [1, 1, 1, 1, 2, 4]
-print(mergeTwoLists(l1, l2))
+    return res
