7ab8b4b8eac0f01d923219f07763b1127e569f25
moongar <moosa.moghimi@gmail.com>
2020-03-17 23:21:39 -0600

    First solution improved. a bit faster

--- Remove Element.py
+++ Remove Element.py
@@ -1,14 +1,14 @@
 class Solution:
     def removeElement(self, nums: list, val: int) -> int:
         count = 0
-        for idx in range(len(nums)):
-            if nums[idx] != val:
-                nums[count] = nums[idx]
+        for n in nums:
+            if n != val:
+                nums[count] = n
                 count += 1
         return count
 
 
 s = Solution()
-nums = [2]
+nums = [2,3,4,3,3,1]
 print(s.removeElement(nums, 3))
 print(nums)
