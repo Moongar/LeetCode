798aa789708e610820e4c5a4e5fd3a940b062b87
moongar <moosa.moghimi@gmail.com>
2020-03-24 22:47:31 -0600

    Second solution using sets, slightly less memory usage and much faster.

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -5,22 +5,31 @@ class Solution:
             return 0
         longest = 1
         start_idx = 0
-        seen = {s[0]: True}
-        while start_idx < len(s) - 1:
-            seen = {s[start_idx]: True}
-            current_len = 1
-            end_idx = start_idx + 1
-            while s[end_idx] not in seen.keys():
-                seen[s[end_idx]] = True
-                current_len += 1
+        # seen = {s[0]: True}
+        # while start_idx < len(s) - 1:
+        #     seen = {s[start_idx]: True}
+        #     current_len = 1
+        #     end_idx = start_idx + 1
+        #     while s[end_idx] not in seen.keys():
+        #         seen[s[end_idx]] = True
+        #         current_len += 1
+        #         end_idx += 1
+        #         if end_idx == len(s):
+        #             return max(current_len, longest)
+        #     longest = max(current_len, longest)
+        #     start_idx += 1
+        # second solution using sets, better memory and much faster. could be faster
+        end_idx = start_idx + 2
+        while end_idx <= len(s):
+            if end_idx - start_idx == len(set(s[start_idx:end_idx])):
+                longest += 1
                 end_idx += 1
-                if end_idx == len(s):
-                    return max(current_len, longest)
-            longest = max(current_len, longest)
-            start_idx += 1
+            else:
+                start_idx += 1
+                end_idx += 1
         return longest
 
 
-input_s = "wew"
+input_s = "weerh"
 s = Solution()
 print(s.lengthOfLongestSubstring(input_s))
