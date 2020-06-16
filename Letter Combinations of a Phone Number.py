ed7b6e0c7b859fd67837c5ef2f67a2c378facfa0
moongar <moosa.moghimi@gmail.com>
2020-06-16 16:57:24 -0600

    Second solution using recursion. a bit slower and less memory efficient than dynamic programming

--- Letter Combinations of a Phone Number.py
+++ Letter Combinations of a Phone Number.py
@@ -12,14 +12,17 @@ class Solution:
         n = len(digits)
         if n == 0:
             return []
-        dp = [[] for x in range(n + 1)]
-        dp[0].append("")
-        for i in range(1, n+1):
-            dp[i] += [y+c for y in dp[i-1] for c in self.letters[digits[i-1]]]
-        return dp[n]
+        if n == 1:
+            return [c for c in self.letters[digits[0]]]
+        # first solution: dynamic programming
+        # dp = [[] for x in range(n + 1)]
+        # dp[0].append("")
+        # for i in range(1, n+1):
+        #     dp[i] += [y+c for y in dp[i-1] for c in self.letters[digits[i-1]]]
+        # return dp[n]
+        # Second solution: recursive
+        return [c+y  for c in self.letters[digits[0]] for y in self.letterCombinations(digits[1:])]
 
 
 s = Solution()
-print(s.letterCombinations("29"))
-m = "a"
-print(m[1:])
+print(s.letterCombinations("23"))
