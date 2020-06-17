51a6fc8a2d5457f7657c4b2b61b73791d5b715d4
moongar <moosa.moghimi@gmail.com>
2020-06-16 23:41:26 -0600

    first solution, average time, but takes up too much memory

--- Permutations.py
+++ Permutations.py
@@ -1,8 +1,16 @@
-def permute(nums):
-    perms =[]
-    
-    return perms
+class Solution:
+    def permute(self, nums):
+        if len(nums) == 0:
+            return [[]]
+        perms = [[] for x in range(len(nums))]
+        perms[0].append([nums[0]])
+        for i in range(1, len(nums)):
+            for c in perms[i-1]:
+                for j in range(len(c)):
+                    perms[i].append(c[0:j]+[nums[i]]+c[j:])
+                perms[i].append(c+[nums[i]])
+        return perms[-1]
 
 
-nums = [1, 2]
-print(permute(nums))
+s= Solution()
+print(s.permute([1, 2, 3]))
