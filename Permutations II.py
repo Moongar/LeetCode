d58e80c3c644fe818c019bd2e6eadb35a29014b3
moongar <moosa.moghimi@gmail.com>
2020-06-17 08:52:45 -0600

    second solution, keeping only two permutations, didn't make any difference

--- Permutations II.py
+++ Permutations II.py
@@ -2,16 +2,19 @@ class Solution:
     def permuteUnique(self, nums):
         if len(nums) == 0:
             return [[]]
-        perms = [[] for x in range(len(nums))]
-        perms[0].append([nums[0]])
-        for i in range(1, len(nums)):
-            for c in perms[i-1]:
+        pre = [[nums[0]]]
+        i = 1
+        while i < len(nums):
+            nxt = []
+            for c in pre:
                 for j in range(len(c)):
-                    if c[0:j] + [nums[i]] + c[j:] not in perms[i]:
-                        perms[i].append(c[0:j] + [nums[i]] + c[j:])
-                if c + [nums[i]] not in perms[i]:
-                    perms[i].append(c + [nums[i]])
-        return perms[-1]
+                    if c[0:j] + [nums[i]] + c[j:] not in nxt:
+                        nxt.append(c[0:j] + [nums[i]] + c[j:])
+                if c + [nums[i]] not in nxt:
+                    nxt.append(c + [nums[i]])
+            i += 1
+            pre = nxt
+        return pre
 
 
 s = Solution()
