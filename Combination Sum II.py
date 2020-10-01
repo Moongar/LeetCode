d7d44c41c6e5eab04b439275924eca37f32daccd
moongar <moosa.moghimi@gmail.com>
2020-09-30 23:05:38 -0600

    First solution using using backtracking, fixed the bug by sorting and now all combinations are unique. Average time

--- Combination Sum II.py
+++ Combination Sum II.py
@@ -1,6 +1,7 @@
 class Solution:
     def combinationSum2(self, candidates, target):
         combs = []
+        candidates.sort()
         self.backtrack(candidates, target, combs, [])
         return combs
 
@@ -11,10 +12,13 @@ class Solution:
         if target < 0:
             return
         for i in range(len(candidates)):
-            self.backtrack(candidates[i + 1:], target - candidates[i], combs, path + [candidates[i]])
+            if i > 0 and candidates[i] == candidates[i-1]:
+                continue
+            else:
+                self.backtrack(candidates[i + 1:], target - candidates[i], combs, path + [candidates[i]])
 
 
 s = Solution()
-cands = [10, 1, 2, 7, 6, 1, 5]
+cands = [10, 1, 2, 7, 6, 1, 5, 1]
 tar = 8
 print(s.combinationSum2(cands, tar))
