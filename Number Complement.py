3e6a8ac74d707f8e75c4cfde7aba47d69ee2d73c
moongar <moosa.moghimi@gmail.com>
2020-10-05 12:02:54 -0600

    First solution using XOR

---
+++ Number Complement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        if num == 0:
+            return 0
+        n = 1
+        while n <= num:
+            n = n << 1
+        return (n - 1) ^ num
+
+
+s = Solution()
+print(s.findComplement(5))
