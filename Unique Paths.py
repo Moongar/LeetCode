69d9d6c7eea190b8eba2ccb9ac916b44cd175929
moongar <moosa.moghimi@gmail.com>
2020-10-06 11:37:33 -0600

    Second solution, bottom up dynamic programming, O(mn) running time. only the last two rows are kept

--- Unique Paths.py
+++ Unique Paths.py
@@ -1,9 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ways = [[1 for c in range(n)] for r in range(m)]
+        # first solution, bottom up dynamic programming O(mn) running time
+        # ways = [[1 for c in range(n)] for r in range(m)]
+        # for r in range(2, m + 1):
+        #     for c in range(2, n + 1):
+        #         ways[m - r][n - c] = ways[m - r + 1][n - c] + ways[m - r][n - c + 1]
+        # return ways[0][0]
+        # second solution, bottom up DP, but we only keep the last two rows to reduce memory usage
+        if m == 1 or n == 1:
+            return 1
+        ways = [[1 for c in range(n)] for r in range(2)]
         for r in range(2, m + 1):
             for c in range(2, n + 1):
-                ways[m - r][n - c] = ways[m - r + 1][n - c] + ways[m - r][n - c + 1]
+                ways[0][n - c] = ways[1][n - c] + ways[0][n - c + 1]
+            ways[1] = ways[0]
         return ways[0][0]
 
 
