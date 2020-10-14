11e576b9e9f1c53a86573b5da30a29e8b4a461f4
moongar <moosa.moghimi@gmail.com>
2020-10-14 13:37:18 -0600

    Second solution, greedy, O(n) time, O(1) memory

--- Jump Game II.py
+++ Jump Game II.py
@@ -4,14 +4,26 @@ class Solution:
             return 0
         elif len(nums) < 3:
             return 1
-        ways = [len(nums) for i in range(len(nums))]
-        ways[0:2] = [0, 1]
-        for i in range(2, len(nums)):
-            for j in reversed(range(i)):
-                if nums[j] != 0:
-                    if nums[j] >= i - j:
-                        ways[i] = min(ways[i], ways[j] + 1)
-        return ways[-1]
+        # first solution, dynamic programming, O(n^2) time, O(n) memory
+        # ways = [len(nums) for i in range(len(nums))]
+        # ways[0:2] = [0, 1]
+        # for i in range(2, len(nums)):
+        #     for j in reversed(range(i)):
+        #         if nums[j] != 0:
+        #             if nums[j] >= i - j:
+        #                 ways[i] = min(ways[i], ways[j] + 1)
+        # return ways[-1]
+        jump_count = 0
+        max_jump = 0
+        must_jump = 0
+        for i in range(len(nums)):
+            max_jump = max(max_jump, i + nums[i])
+            if max_jump >= len(nums) - 1:
+                return jump_count + 1
+            if i == must_jump:
+                must_jump = max_jump
+                jump_count += 1
+        return jump_count
 
 
 s = Solution()
